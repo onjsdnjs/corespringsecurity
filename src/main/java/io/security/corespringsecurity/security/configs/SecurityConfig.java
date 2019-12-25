e9cc7af85c6c4edb09c84129172570ead4ce0106
onjsdnjs <onjsdnjs@gmail.com>
2019-12-25 20:59:09 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         http
                 .authorizeRequests()
+                .antMatchers("/","/users").permitAll()
                 .anyRequest().authenticated()
 
         .and()
