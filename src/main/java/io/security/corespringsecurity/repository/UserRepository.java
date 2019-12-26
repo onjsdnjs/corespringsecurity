3c1fbd652480cc1fda56dc3ba5ffd1a713a4d656
onjsdnjs <onjsdnjs@gmail.com>
2019-12-26 21:22:15 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/repository/UserRepository.java
+++ src/main/java/io/security/corespringsecurity/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Account, Long> {
   Account findByUsername(String username);
+  int countByUsername(String username);
 }
