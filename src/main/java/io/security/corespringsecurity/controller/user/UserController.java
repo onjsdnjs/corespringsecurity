63126f56583920566f2f504fbc6fed4462b3c426
onjsdnjs <onjsdnjs@gmail.com>
2019-12-27 20:50:08 +0900

    lecture'

--- src/main/java/io/security/corespringsecurity/controller/user/UserController.java
+++ src/main/java/io/security/corespringsecurity/controller/user/UserController.java
@@ -2,12 +2,13 @@ package io.security.corespringsecurity.controller.user;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 	
-	@PostMapping(value="/mypage")
+	@GetMapping(value="/mypage")
 	public String myPage() throws Exception {
 
 		return "user/mypage";
