59baa149b1bd1e484d1bf9389d645af1154aece6
onjsdnjs <onjsdnjs@gmail.com>
2019-12-28 18:54:08 +0900

    lecture

---
+++ src/main/java/io/security/corespringsecurity/security/exception/AuthMethodNotSupportedException.java
@@ -0,0 +1,11 @@
+package io.security.corespringsecurity.security.exception;
+
+import org.springframework.security.authentication.AuthenticationServiceException;
+
+public class AuthMethodNotSupportedException extends AuthenticationServiceException {
+    private static final long serialVersionUID = 3705043083010304496L;
+
+    public AuthMethodNotSupportedException(String msg) {
+        super(msg);
+    }
+}
