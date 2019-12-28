e890be7ed0d5ff3ad303ffe62018c83b2c920639
onjsdnjs <onjsdnjs@gmail.com>
2019-12-28 16:00:06 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
+++ src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -1,14 +1,8 @@
 package io.security.corespringsecurity.controller.login;
 
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -18,14 +12,4 @@ public class LoginController {
 		return "login";
 	}
 
-	@GetMapping(value = "/logout")
-	public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication != null){
-			new SecurityContextLogoutHandler().logout(request, response, authentication);
-		}
-
-		return "redirect:/login";
-	}
 }
