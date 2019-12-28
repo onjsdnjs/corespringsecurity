aa0c67c51ce45ba4a32a1fdf7ce0ae6f03f8cd1c
onjsdnjs <onjsdnjs@gmail.com>
2019-12-28 17:58:37 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/controller/user/UserController.java
+++ src/main/java/io/security/corespringsecurity/controller/user/UserController.java
@@ -3,14 +3,20 @@ package io.security.corespringsecurity.controller.user;
 
 import io.security.corespringsecurity.domain.dto.AccountDto;
 import io.security.corespringsecurity.domain.entity.Account;
+import io.security.corespringsecurity.security.service.AccountContext;
 import io.security.corespringsecurity.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.Principal;
 import java.util.Arrays;
 
 @Controller
@@ -40,7 +46,22 @@ public class UserController {
 	}
 
 	@GetMapping(value="/mypage")
-	public String myPage() throws Exception {
+	public String myPage(@AuthenticationPrincipal Account account, Authentication authentication, Principal principal) throws Exception {
+
+		String username1 = account.getUsername();
+		System.out.println("username1 = " + username1);
+
+		Account account2 = (Account) authentication.getPrincipal();
+		String username2 = account2.getUsername();
+		System.out.println("username2 = " + username2);
+
+		Account account3 = (Account)((UsernamePasswordAuthenticationToken)principal).getPrincipal();
+		String username3 = account3.getUsername();
+		System.out.println("username3 = " + username3);
+
+		Account account4 = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username4 = account4.getUsername();
+		System.out.println("username4 = " + username4);
 
 		return "user/mypage";
 	}
--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
