9dd376f81898748307e392297936f0f3828c9e92
onjsdnjs <onjsdnjs@gmail.com>
2020-01-01 18:43:48 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/aopsecurity/AopSecurityController.java
+++ src/main/java/io/security/corespringsecurity/aopsecurity/AopSecurityController.java
@@ -1,5 +1,8 @@
 package io.security.corespringsecurity.aopsecurity;
 
+import io.security.corespringsecurity.aopsecurity.service.AopLiveMethodService;
+import io.security.corespringsecurity.aopsecurity.service.AopMethodService;
+import io.security.corespringsecurity.aopsecurity.service.AopPointcutService;
 import io.security.corespringsecurity.domain.dto.AccountDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/io/security/corespringsecurity/aopsecurity/service/AopLiveMethodService.java
+++ src/main/java/io/security/corespringsecurity/aopsecurity/service/AopLiveMethodService.java
@@ -1,4 +1,4 @@
-package io.security.corespringsecurity.aopsecurity;
+package io.security.corespringsecurity.aopsecurity.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
