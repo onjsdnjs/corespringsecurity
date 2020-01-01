b3e56232cf3cd688e5ce83b4dc2a1f0b730652d2
onjsdnjs <onjsdnjs@gmail.com>
2020-01-01 13:52:41 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/controller/HomeController.java
+++ src/main/java/io/security/corespringsecurity/controller/HomeController.java
@@ -1,6 +1,8 @@
 package io.security.corespringsecurity.controller;
 
 
+import io.security.corespringsecurity.domain.dto.AccountDto;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,4 +14,9 @@ public class HomeController {
 		return "home";
 	}
 
+	@GetMapping("/preAuthorize")
+	@PreAuthorize("hasRole('ROLE_USER') or (#account.username == principal.name)")
+	public void preAuthorize(AccountDto account){
+		System.out.println("account.username = " + account.getUsername() + "");
+	}
 }
