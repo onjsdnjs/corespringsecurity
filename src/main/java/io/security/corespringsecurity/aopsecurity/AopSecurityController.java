fbe41ebfd4d4acb61453f763988cfa1fc37707eb
onjsdnjs <onjsdnjs@gmail.com>
2020-01-01 16:16:03 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/aopsecurity/AopSecurityController.java
+++ src/main/java/io/security/corespringsecurity/aopsecurity/AopSecurityController.java
@@ -2,6 +2,7 @@ package io.security.corespringsecurity.aopsecurity;
 
 import io.security.corespringsecurity.domain.dto.AccountDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +12,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class AopSecurityController {
 
+    @Autowired
+    private AopMethodService aopMethodService;
+
     @GetMapping("/preAuthorize")
     @PreAuthorize("hasRole('ROLE_USER') AND #account.username == principal.username")
     public String preAuthorize(AccountDto account, Model model){
 
         System.out.println("account.username = " + account.getUsername() + "");
         model.addAttribute("method", "Success @PreAuthorize");
+
+        return "aop/method";
+    }
+
+    @GetMapping("/notMethodSecured")
+    public String methodSecured(Model model){
+
+        aopMethodService.methodSecured();
+        model.addAttribute("method", "Success MethodSecured");
 
         return "aop/method";
     }
