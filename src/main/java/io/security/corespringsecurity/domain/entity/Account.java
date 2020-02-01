9f714f7f0977c369e3fc97a0143e3c2a41b04e16
leaven <leaven73@gmail.com>
2020-02-01 19:37:24 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/domain/entity/Account.java
+++ src/main/java/io/security/corespringsecurity/domain/entity/Account.java
@@ -18,20 +18,10 @@ public class Account implements Serializable {
     @Id
     @GeneratedValue
     private Long id;
-
-    @Column
     private String username;
-
-    @Column
     private String email;
-
-    @Column
     private int age;
-
-    @Column
     private String password;
-
-    @Column
     private String role;
 }
 
