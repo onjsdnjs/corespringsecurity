3b9bc22260f1a1a9a76f27db2b26d999b24092dc
leaven <leaven73@gmail.com>
2020-02-04 17:00:40 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/domain/entity/Account.java
+++ src/main/java/io/security/corespringsecurity/domain/entity/Account.java
@@ -8,12 +8,13 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Account {
+public class Account implements Serializable {
 
     @Id
     @GeneratedValue
