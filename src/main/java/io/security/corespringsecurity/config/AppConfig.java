ff309a3324deeb6f5ff8f5ced1e8471fdab8b9d6
leaven <leaven73@gmail.com>
2020-02-13 10:23:48 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/config/AppConfig.java
+++ src/main/java/io/security/corespringsecurity/config/AppConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@AutoConfigureBefore({MethodSecurityConfig.class})
 public class AppConfig {
 
     @Bean
