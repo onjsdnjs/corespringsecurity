a5451dbe6a8adb1ef8b4d3e96014336b7d1cd299
onjsdnjs <onjsdnjs@gmail.com>
2020-02-21 00:07:58 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,28 +1,23 @@
 package io.security.corespringsecurity.security.configs;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebSecurity
-@Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/images/**").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().authenticated();
 
-        .and()
-                .formLogin();
+        http
+                .formLogin()
 
-
+                ;
     }
 }
