9b7d126d443671d17208c7027e28ec934a361b73
onjsdnjs <onjsdnjs@gmail.com>
2020-02-23 19:23:44 +0900

    lecture'

--- src/main/java/io/security/corespringsecurity/repository/ResourcesRepository.java
+++ src/main/java/io/security/corespringsecurity/repository/ResourcesRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public interface ResourcesRepository extends JpaRepository<Resources, Long> {
 
+    Resources findByResourceNameAndHttpMethod(String resourceName, String httpMethod);
+
     @Query("select r from Resources r join fetch r.roleSet where r.resourceType = 'url' order by r.orderNum desc")
     List<Resources> findAllResources();
 }
