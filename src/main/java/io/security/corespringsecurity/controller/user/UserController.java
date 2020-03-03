d8f17af48f3a82afaedb82fe1aac6ab4774a956a
leaven <leaven73@gmail.com>
2020-03-03 16:40:02 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/controller/user/UserController.java
+++ src/main/java/io/security/corespringsecurity/controller/user/UserController.java
@@ -3,14 +3,12 @@ package io.security.corespringsecurity.controller.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 	
 	@GetMapping(value="/mypage")
 	public String myPage() throws Exception {
-
 		return "user/mypage";
 	}
 }
