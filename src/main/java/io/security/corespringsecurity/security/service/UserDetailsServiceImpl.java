f3a44829df1f554d6ec772fc942a6ad60312371a
onjsdnjs <onjsdnjs@gmail.com>
2020-03-05 00:26:44 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/security/service/UserDetailsServiceImpl.java
+++ src/main/java/io/security/corespringsecurity/security/service/UserDetailsServiceImpl.java
@@ -35,12 +35,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 throw new UsernameNotFoundException("No user found with username: " + username);
             }
         }
-        Set<String> userRoles = account.getUserRoles()
+        List<GrantedAuthority> collect = account.getUserRoles()
                 .stream()
                 .map(userRole -> userRole.getRoleName())
-                .collect(Collectors.toSet());
+                .collect(Collectors.toSet())
+                .stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
 
-        List<GrantedAuthority> collect = userRoles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+        //List<GrantedAuthority> collect = userRoles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
         return new AccountContext(account, collect);
     }
 }
