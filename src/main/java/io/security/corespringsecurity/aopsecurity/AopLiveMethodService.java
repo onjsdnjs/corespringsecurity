d240ad679e30616bdf0b8a8baee7f18f267c5df6
onjsdnjs <onjsdnjs@gmail.com>
2020-03-07 20:47:24 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/aopsecurity/AopLiveMethodService.java
+++ src/main/java/io/security/corespringsecurity/aopsecurity/AopLiveMethodService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 public class AopLiveMethodService {
 
     public void liveMethodSecured(){
+
         System.out.println("liveMethodSecured");
     }
 }
--- src/main/java/io/security/corespringsecurity/service/MethodSecurityService.java
+++ src/main/java/io/security/corespringsecurity/service/MethodSecurityService.java
@@ -1,18 +1,21 @@
 package io.security.corespringsecurity.service;
 
 import io.security.corespringsecurity.security.aop.CustomMethodSecurityInterceptor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.access.intercept.aopalliance.MethodSecurityInterceptor;
+import org.springframework.security.access.intercept.aopalliance.MethodSecurityMetadataSourceAdvisor;
 import org.springframework.security.access.method.MapBasedMethodSecurityMetadataSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ClassUtils;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class MethodSecurityService {
@@ -20,6 +23,10 @@ public class MethodSecurityService {
     private MapBasedMethodSecurityMetadataSource mapBasedMethodSecurityMetadataSource;
     private AnnotationConfigServletWebServerApplicationContext applicationContext;
     private CustomMethodSecurityInterceptor methodSecurityInterceptor;
+
+    private Map<String, Object> proxyMap = new HashMap<>();
+    private Map<String, ProxyFactory> advisedMap = new HashMap<>();
+    private Map<String, Object> targetMap = new HashMap<>();
 
     public MethodSecurityService(MapBasedMethodSecurityMetadataSource mapBasedMethodSecurityMetadataSource, AnnotationConfigServletWebServerApplicationContext applicationContext, CustomMethodSecurityInterceptor methodSecurityInterceptor) {
         this.mapBasedMethodSecurityMetadataSource = mapBasedMethodSecurityMetadataSource;
@@ -35,18 +42,44 @@ public class MethodSecurityService {
         Class<?> type = ClassUtils.resolveClassName(typeName, ClassUtils.getDefaultClassLoader());
         String beanName = type.getSimpleName().substring(0, 1).toLowerCase() + type.getSimpleName().substring(1);
 
-        ProxyFactory proxyFactory = new ProxyFactory();
-        proxyFactory.setTarget(type.getDeclaredConstructor().newInstance());
-        proxyFactory.addAdvice(methodSecurityInterceptor);
-        Object proxy = proxyFactory.getProxy();
+        ProxyFactory proxyFactory = advisedMap.get(beanName);
+        Object target = targetMap.get(beanName);
 
-        List<ConfigAttribute> attr = Arrays.asList(new SecurityConfig(roleName));
-        mapBasedMethodSecurityMetadataSource.addSecureMethod(type,methodName, attr);
+        if(proxyFactory == null){
 
-        DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry)applicationContext.getBeanFactory();
-        registry.destroySingleton(beanName);
-        registry.registerSingleton(beanName, proxy);
+            proxyFactory = new ProxyFactory();
+            if(target == null) {
+                proxyFactory.setTarget(type.getDeclaredConstructor().newInstance());
 
+            }else{
+                proxyFactory.setTarget(target);
+            }
+            proxyFactory.addAdvice(methodSecurityInterceptor);
+
+            advisedMap.put(beanName, proxyFactory);
+
+        }else{
+
+            int adviceIndex = proxyFactory.indexOf(methodSecurityInterceptor);
+            if(adviceIndex == -1){
+                proxyFactory.addAdvice(methodSecurityInterceptor);
+            }
+        }
+
+        Object proxy = proxyMap.get(beanName);
+
+        if(proxy == null){
+
+            proxy = proxyFactory.getProxy();
+            proxyMap.put(beanName, proxy);
+
+            List<ConfigAttribute> attr = Arrays.asList(new SecurityConfig(roleName));
+            mapBasedMethodSecurityMetadataSource.addSecureMethod(type,methodName, attr);
+
+            DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry)applicationContext.getBeanFactory();
+            registry.destroySingleton(beanName);
+            registry.registerSingleton(beanName, proxy);
+        }
     }
 
     public void removeMethodSecured(String className) throws Exception{
@@ -58,9 +91,16 @@ public class MethodSecurityService {
         Object newInstance = type.getDeclaredConstructor().newInstance();
 
         DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry)applicationContext.getBeanFactory();
-        Object singleton = registry.getSingleton(beanName);
-        registry.destroySingleton(beanName);
-        registry.registerSingleton(beanName, newInstance);
 
+        ProxyFactory proxyFactory = advisedMap.get(beanName);
+
+        if(proxyFactory != null){
+            proxyFactory.removeAdvice(methodSecurityInterceptor);
+
+        }else{
+            registry.destroySingleton(beanName);
+            registry.registerSingleton(beanName, newInstance);
+            targetMap.put(beanName,newInstance);
+        }
     }
 }
