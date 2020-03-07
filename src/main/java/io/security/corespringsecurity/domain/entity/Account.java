95c386f2024b1763677ef2a87f65891545d28b74
onjsdnjs <onjsdnjs@gmail.com>
2020-03-07 20:51:01 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/domain/entity/Account.java
+++ src/main/java/io/security/corespringsecurity/domain/entity/Account.java
@@ -33,7 +33,7 @@ public class Account implements Serializable {
     private String password;
 
     @ManyToMany(fetch = FetchType.LAZY, cascade={CascadeType.ALL})
-    @JoinTable(name = "user_roles", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = {
+    @JoinTable(name = "account_roles", joinColumns = { @JoinColumn(name = "account_id") }, inverseJoinColumns = {
             @JoinColumn(name = "role_id") })
     private Set<Role> userRoles = new HashSet<>();
 }
